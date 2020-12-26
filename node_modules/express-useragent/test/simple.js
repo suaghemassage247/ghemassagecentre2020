941aabecb8acf017ad85e10466ca5d66973e0ab9
giagocgiasoc <giagocgiasoc@gmail.com>
2020-12-26 12:01:47 +0700

    update

--- node_modules/express-useragent/test/simple.js
+++ node_modules/express-useragent/test/simple.js
@@ -4,10 +4,10 @@
  * @created     2012-03-24 16:21:10
  * @category    Express Helpers
  * @package     express-useragent
- * @version     0.0.4
+ * @version     1.0.14
  * @copyright   Copyright (c) 2009-2012 - All rights reserved.
  * @license     MIT License
- * @author      Alexey Gordeyev IK <aleksej@gordejev.lv>
+ * @author      Aleksejs Gordejevs IK <aleksej@gordejev.lv>
  * @link        http://www.gordejev.lv
  *
  */
